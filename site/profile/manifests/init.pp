395f707a032c706bcd566aae5581e246ad496635
Cody Herriges <cody@herriges.org>
2015-09-22 22:14:29 -0700

    Sets up ntp on all hosts.
      Simple inclusion of ntp on all hosts so it is consistent.

--- site/profile/manifests/init.pp
+++ site/profile/manifests/init.pp
@@ -6,5 +6,14 @@ class profile {
   include('::profile::sudo')
   include('::profile::ssh')
 
+  class { '::ntp':
+    servers => [
+      '0.north-america.pool.ntp.org',
+      '1.north-america.pool.ntp.org',
+      '2.north-america.pool.ntp.org',
+      '3.north-america.pool.ntp.org',
+    ]
+  }
+
   package { 'zsh': ensure => present }
 }
