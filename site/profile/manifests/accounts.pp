c5d78c95bd7a16e3ccfdf219fd860de4c138b531
Cody Herriges <cody@herriges.org>
2015-11-30 22:02:54 -0800

    Make sure users exists
      Put the basic group in place for my user to access things.

--- site/profile/manifests/accounts.pp
+++ site/profile/manifests/accounts.pp
@@ -5,6 +5,12 @@ class profile::accounts {
   include('::profile::accounts::distributor')
   Class['::profile::accounts::distributor'] -> Class['::profile::accounts']
 
+  group { 'users':
+    ensure => present,
+    system => true,
+    gid    => '100',
+  }
+
   profile::accounts::user { 'ody':
     gecos  => 'Cody Herriges',
     groups => ['users', 'wheel'],
